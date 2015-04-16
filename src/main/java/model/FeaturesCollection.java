c6827f85ca0764b7595cd9e7c36415f8a711eb15
iyaruta <i.yaruta@hotmail.com>
2015-04-16 21:01:49 +0300

    Add global model

---
+++ src/main/java/model/FeaturesCollection.java
@@ -0,0 +1,23 @@
+package model;
+
+public class FeaturesCollection {
+
+    private String type;
+    private Feature[] features;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Feature[] getFeatures() {
+        return features;
+    }
+
+    public void setFeatures(Feature[] features) {
+        this.features = features;
+    }
+}
